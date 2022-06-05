e187e056cb4d786f3720abe971838dace0cc6838
Samuel Colvin <s@muelcolvin.com>
2022-06-05 11:03:59 +0100

    messing with simd

--- benches/main.rs
+++ benches/main.rs
@@ -1,59 +1,63 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use int_bench::{fast_parse1, fast_parse2};
+use int_bench::{fast_parse1, simd};
 
 fn bench(c: &mut Criterion) {
-    let pos_int0 = black_box("0");
-    let pos_int1 = black_box("1");
-    let pos_int123 = black_box("123");
-    let pos_int_mid = black_box("12345678901234");
-    let pos_int_long = black_box("1234567890123456");
-    let neg_int0 = black_box("-0");
-    let neg_int1 = black_box("-1");
-    let neg_int123 = black_box("-123");
-    let neg_int_mid = black_box("-12345678901234");
-    let neg_int_long = black_box("-1234567890123456");
+    let pos_int0 = black_box("1");
+    let pos_int1 = black_box("0012");
+    let pos_int123 = black_box("1234");
+    let pos_int_mid = black_box("12345678");
+    let pos_int_long = black_box("99999999");
+    // let pos_int_mid = black_box("12345678901234");
+    // let pos_int_long = black_box("1234567890123456");
+    // let neg_int0 = black_box("-0");
+    // let neg_int1 = black_box("-1");
+    // let neg_int123 = black_box("-123");
+    // let neg_int_mid = black_box("-12345678901234");
+    // let neg_int_long = black_box("-1234567890123456");
 
-    c.bench_function("std", |b| b.iter(|| {
-        black_box(str::parse::<i64>(pos_int0).unwrap());
-        black_box(str::parse::<i64>(pos_int1).unwrap());
-        black_box(str::parse::<i64>(pos_int123).unwrap());
-        black_box(str::parse::<i64>(pos_int_mid).unwrap());
-        black_box(str::parse::<i64>(pos_int_long).unwrap());
-        black_box(str::parse::<i64>(neg_int0).unwrap());
-        black_box(str::parse::<i64>(neg_int1).unwrap());
-        black_box(str::parse::<i64>(neg_int123).unwrap());
-        black_box(str::parse::<i64>(neg_int_mid).unwrap());
-        black_box(str::parse::<i64>(neg_int_long).unwrap());
-    }));
+    c.bench_function("std", |b| {
+        b.iter(|| {
+            black_box(str::parse::<i64>(pos_int0).unwrap());
+            black_box(str::parse::<i64>(pos_int1).unwrap());
+            black_box(str::parse::<i64>(pos_int123).unwrap());
+            black_box(str::parse::<i64>(pos_int_mid).unwrap());
+            black_box(str::parse::<i64>(pos_int_long).unwrap());
 
-    c.bench_function("fast1", |b| b.iter(|| {
-        black_box(fast_parse1(pos_int0).unwrap());
-        black_box(fast_parse1(pos_int1).unwrap());
-        black_box(fast_parse1(pos_int123).unwrap());
-        black_box(fast_parse1(pos_int_mid).unwrap());
-        black_box(fast_parse1(pos_int_long).unwrap());
+            // black_box(str::parse::<i64>(neg_int0).unwrap());
+            // black_box(str::parse::<i64>(neg_int1).unwrap());
+            // black_box(str::parse::<i64>(neg_int123).unwrap());
+            // black_box(str::parse::<i64>(neg_int_mid).unwrap());
 
-        black_box(fast_parse1(neg_int0).unwrap());
-        black_box(fast_parse1(neg_int1).unwrap());
-        black_box(fast_parse1(neg_int123).unwrap());
-        black_box(fast_parse1(neg_int_mid).unwrap());
-        black_box(fast_parse1(neg_int_long).unwrap());
-    }));
+            // black_box(str::parse::<i64>(neg_int_long).unwrap());
+        })
+    });
 
-    c.bench_function("fast2", |b| b.iter(|| {
-        black_box(fast_parse2(pos_int0).unwrap());
-        black_box(fast_parse2(pos_int1).unwrap());
-        black_box(fast_parse2(pos_int123).unwrap());
-        black_box(fast_parse2(pos_int_mid).unwrap());
-        black_box(fast_parse2(pos_int_long).unwrap());
+    c.bench_function("fast1", |b| {
+        b.iter(|| {
+            black_box(fast_parse1(pos_int0).unwrap());
+            black_box(fast_parse1(pos_int1).unwrap());
+            black_box(fast_parse1(pos_int123).unwrap());
+            black_box(fast_parse1(pos_int_mid).unwrap());
+            black_box(fast_parse1(pos_int_long).unwrap());
 
-        black_box(fast_parse2(neg_int0).unwrap());
-        black_box(fast_parse2(neg_int1).unwrap());
-        black_box(fast_parse2(neg_int123).unwrap());
-        black_box(fast_parse2(neg_int_mid).unwrap());
-        black_box(fast_parse2(neg_int_long).unwrap());
-    }));
+            // black_box(fast_parse1(neg_int0).unwrap());
+            // black_box(fast_parse1(neg_int1).unwrap());
+            // black_box(fast_parse1(neg_int123).unwrap());
+            // black_box(fast_parse1(neg_int_mid).unwrap());
+            // black_box(fast_parse1(neg_int_long).unwrap());
+        })
+    });
+
+    c.bench_function("simd", |b| {
+        b.iter(|| {
+            black_box(simd(pos_int0.as_bytes()).unwrap());
+            black_box(simd(pos_int1.as_bytes()).unwrap());
+            black_box(simd(pos_int123.as_bytes()).unwrap());
+            black_box(simd(pos_int_mid.as_bytes()).unwrap());
+            black_box(simd(pos_int_long.as_bytes()).unwrap());
+        })
+    });
 }
 
 criterion_group!(arbitrary, bench);
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,85 @@
 #![feature(test)]
+#![feature(slice_ptr_get)]
 
 extern crate test;
+use core::arch::aarch64::*;
 
-fn main() {
-    println!("18446744073709551615: {}", fast_parse("18446744073709551615").unwrap());
+pub fn simd(bytes: &[u8]) -> Option<u64> {
+    unsafe {
+        let s: uint8x8_t = match bytes.len() {
+            1 => return Some((bytes[0] & 0x0f) as u64),
+            2 => return Some((bytes[0] & 0x0f) as u64 * 10 + (bytes[1] & 0x0f) as u64),
+            // 3 => return (bytes[0] & 0x0f) as u64 * 100 + (bytes[1] & 0x0f) as u64 * 10 + (bytes[2] & 0x0f) as u64,
+            3 => {
+                let bytes8: [u8; 4] = [b'0', bytes[0], bytes[1], bytes[2]];
+                return simd4(vld1_u8(bytes8.as_ptr()))
+            }
+            4 => return simd4(vld1_u8(bytes.as_ptr())),
+            5 => {
+                let bytes8: [u8; 8] = [b'0', b'0', b'0', bytes[0], bytes[1], bytes[2], bytes[3], bytes[4]];
+                vld1_u8(bytes8.as_ptr())
+            }
+            6 => {
+                let bytes8: [u8; 8] = [b'0', b'0', bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5]];
+                vld1_u8(bytes8.as_ptr())
+            }
+            7 => {
+                let bytes8: [u8; 8] = [
+                    b'0', bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6],
+                ];
+                vld1_u8(bytes8.as_ptr())
+            }
+            8 => vld1_u8(bytes.as_ptr()),
+            _ => return None,
+        };
+
+        let zeros = vld1_u8([b'0'; 8].as_ptr());
+        let mut ok: u8 = 0;
+        let gte_0 = vcge_u8(s, zeros);
+        vst1_u8(&mut ok, gte_0);
+        if ok == 0 {
+            return None
+        }
+        let lte_9 = vcle_u8(s, vld1_u8([b'9'; 8].as_ptr()));
+        vst1_u8(&mut ok, lte_9);
+        if ok == 0 {
+            return None
+        }
+
+        let v = vsub_u8(s, zeros);
+
+        let v = vmul_u8(v, vld1_u8([10_u8, 1_u8, 10_u8, 1_u8, 10_u8, 1_u8, 10_u8, 1_u8].as_ptr()));
+
+        let v = vpaddl_u8(v);
+        let v = vmul_u16(v, vld1_u16([100_u16, 1_u16, 100_u16, 1_u16].as_ptr()));
+
+        let v = vpaddl_u16(v);
+        let v = vmul_u32(v, vld1_u32([10_000_u32, 1_u32].as_ptr()));
+
+        let v = vpaddl_u32(v);
+
+        let mut result: u64 = 0;
+        vst1_u64(&mut result, v);
+        return Some(result);
+    }
 }
+
+pub fn simd4(s: uint8x8_t) -> Option<u64> {
+    unsafe {
+        let v = vsub_u8(s, vld1_u8([b'0'; 4].as_ptr()));
+        let v = vmul_u8(v, vld1_u8([10_u8, 1_u8, 10_u8, 1_u8].as_ptr()));
+
+        let v = vpaddl_u8(v);
+        let v = vmul_u16(v, vld1_u16([100_u16, 1_u16, 0_u16, 0_u16].as_ptr()));
+
+        let v = vpaddl_u16(v);
+
+        let mut result: u32 = 0;
+        vst1_u32(&mut result, v);
+        return Some(result as u64);
+    }
+}
+
 
 pub fn fast_parse1(s: &str) -> Option<i64> {
     let mut bytes = s.bytes();
@@ -48,7 +123,7 @@ pub fn fast_parse2(s: &str) -> Option<i64> {
             c if (b'0'..=b'9').contains(c) => {
                 result = (c & 0x0f) as u64;
                 true
-            },
+            }
             _ => return None,
         };
 
@@ -74,31 +149,51 @@ pub fn fast_parse2(s: &str) -> Option<i64> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{fast_parse1, fast_parse2};
+    #[test]
+    fn simd() {
+        assert_eq!(super::simd(b"1").unwrap(), 1);
+        assert_eq!(super::simd(b"12").unwrap(), 12);
+        assert_eq!(super::simd(b"0012").unwrap(), 12);
+        assert_eq!(super::simd(b"123").unwrap(), 123);
+        assert_eq!(super::simd(b"1234").unwrap(), 1234);
+        assert_eq!(super::simd(b"12345").unwrap(), 12345);
+        assert_eq!(super::simd(b"123456").unwrap(), 123456);
+        assert_eq!(super::simd(b"1234567").unwrap(), 1234567);
+        assert_eq!(super::simd(b"12345678").unwrap(), 12345678);
+        assert_eq!(super::simd(b"99999999").unwrap(), 99999999);
+        assert_eq!(super::simd(b"!9999999"), None);
+        assert_eq!(super::simd(b"a9999999"), None);
+    }
 
     #[test]
     fn test_fast_parse1() {
-        assert_eq!(fast_parse1("0").unwrap(), 0);
-        assert_eq!(fast_parse1("1").unwrap(), 1);
-        assert_eq!(fast_parse1("-1").unwrap(), -1);
-        assert_eq!(fast_parse1("123").unwrap(), 123);
-        assert_eq!(fast_parse1("-123").unwrap(), -123);
-        assert_eq!(fast_parse1("1585201087123789").unwrap(), 1585201087123789);
-        assert_eq!(fast_parse1("1234567890123456").unwrap(), 1234567890123456);
-        assert_eq!(fast_parse1("1111111111111111").unwrap(), 1111111111111111);
-        assert_eq!(fast_parse1(&(i64::MAX as u64 - 1).to_string()).unwrap(), 9223372036854775806);
+        assert_eq!(super::fast_parse1("0").unwrap(), 0);
+        assert_eq!(super::fast_parse1("1").unwrap(), 1);
+        assert_eq!(super::fast_parse1("-1").unwrap(), -1);
+        assert_eq!(super::fast_parse1("123").unwrap(), 123);
+        assert_eq!(super::fast_parse1("-123").unwrap(), -123);
+        assert_eq!(super::fast_parse1("1585201087123789").unwrap(), 1585201087123789);
+        assert_eq!(super::fast_parse1("1234567890123456").unwrap(), 1234567890123456);
+        assert_eq!(super::fast_parse1("1111111111111111").unwrap(), 1111111111111111);
+        assert_eq!(
+            super::fast_parse1(&(i64::MAX as u64 - 1).to_string()).unwrap(),
+            9223372036854775806
+        );
     }
 
     #[test]
     fn test_fast_parse2() {
-        assert_eq!(fast_parse2("0").unwrap(), 0);
-        assert_eq!(fast_parse2("1").unwrap(), 1);
-        assert_eq!(fast_parse2("-1").unwrap(), -1);
-        assert_eq!(fast_parse2("123").unwrap(), 123);
-        assert_eq!(fast_parse2("-123").unwrap(), -123);
-        assert_eq!(fast_parse2("1585201087123789").unwrap(), 1585201087123789);
-        assert_eq!(fast_parse2("1234567890123456").unwrap(), 1234567890123456);
-        assert_eq!(fast_parse2("1111111111111111").unwrap(), 1111111111111111);
-        assert_eq!(fast_parse2(&(i64::MAX as u64 - 1).to_string()).unwrap(), 9223372036854775806);
+        assert_eq!(super::fast_parse2("0").unwrap(), 0);
+        assert_eq!(super::fast_parse2("1").unwrap(), 1);
+        assert_eq!(super::fast_parse2("-1").unwrap(), -1);
+        assert_eq!(super::fast_parse2("123").unwrap(), 123);
+        assert_eq!(super::fast_parse2("-123").unwrap(), -123);
+        assert_eq!(super::fast_parse2("1585201087123789").unwrap(), 1585201087123789);
+        assert_eq!(super::fast_parse2("1234567890123456").unwrap(), 1234567890123456);
+        assert_eq!(super::fast_parse2("1111111111111111").unwrap(), 1111111111111111);
+        assert_eq!(
+            super::fast_parse2(&(i64::MAX as u64 - 1).to_string()).unwrap(),
+            9223372036854775806
+        );
     }
 }
